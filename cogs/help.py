b6d43207aa7ada0aebb4ac8fc5a60ed9c963fd1f
StarNumber12046 <francesco.lumaca2009@gmail.com>
2021-03-11 10:00:51 +0100

    added the modular prefix

--- cogs/help.py
+++ cogs/help.py
@@ -9,9 +9,9 @@ class Help(commands.Cog):
     @commands.group(invoke_without_command=True)
     async def help(self, ctx):
         embed = discord.Embed(title='Aiuto', description=None, color=discord.Color.blurple())
-        embed.add_field(name='Moderazione', value='r-help moderator | Modera il tuo server', inline=False)
-        embed.add_field(name='Misc', value='r-help misc | Comandi inutili ma divertenti', inline=False)
-        embed.add_field(name='Info', value='r-help info | Info sul bot', inline=False)
+        embed.add_field(name='Moderazione', value=f'{ctx.prefix}help moderator | Modera il tuo server', inline=False)
+        embed.add_field(name='Misc', value=f'{ctx.prefix}help misc | Comandi inutili ma divertenti', inline=False)
+        embed.add_field(name='Info', value=f'{ctx.prefix}help info | Info sul bot', inline=False)
         embed.set_footer(text='C\'è un comando segreto che devi assolutamente trovare! E se lo trovi dillo al mio '
                               'owner StarNumber12046#9008')
         await ctx.send(embed=embed)
@@ -19,17 +19,17 @@ class Help(commands.Cog):
     @help.command()
     async def moderator(self, ctx):
         embed = discord.Embed(title='Moderazione', description='<> = non obbligatorio | [] = obbligatorio', color=discord.Color.blurple())
-        embed.add_field(name='ban', value='r-ban [user] <reason> (Autorizzazione richiesta: bannare membri)', inline=False)
-        embed.add_field(name='kick', value='r-kick [user] <reason> (Autorizzazione richiesta: espellere membri)', inline=False)
-        embed.add_field(name='unban', value='r-unban [user] (Autorizzazione richiesta: bannare membri)', inline=False)
+        embed.add_field(name='ban', value=f'{ctx.prefix}ban [user] <reason> (Autorizzazione richiesta: bannare membri)', inline=False)
+        embed.add_field(name='kick', value=f'{ctx.prefix}kick [user] <reason> (Autorizzazione richiesta: espellere membri)', inline=False)
+        embed.add_field(name='unban', value=f'{ctx.prefix}unban [user] (Autorizzazione richiesta: bannare membri)', inline=False)
 
         await ctx.send(embed=embed)
 
     @help.command()
     async def info(self, ctx):
         embed = discord.Embed(title='Info sul bot', description='[] = obbligatorio, <> = non obbligatorio', color=discord.Color.blurple())
-        embed.add_field(name='Invite', value='r-invite | invitami', inline=False)
-        embed.add_field(name='Support', value='r-support | server di supporto', inline=False)
+        embed.add_field(name='Invite', value=f'{ctx.prefix}invite | invitami', inline=False)
+        embed.add_field(name='Support', value=f'{ctx.prefix}support | server di supporto', inline=False)
         await ctx.send(embed=embed)
 
 
@@ -37,13 +37,13 @@ class Help(commands.Cog):
     @help.command()
     async def misc(self, ctx):
         embed = discord.Embed(title='Misc', description='[] = obbligatorio | <> = non obbligatorio', color=discord.Color.blurple())
-        embed.add_field(name='userinfo', value='r-userinfo  <utente> (Se non si inserisce un utente il bot restituirà le info dell\'autore del comando)',
+        embed.add_field(name='userinfo', value=f'{ctx.prefix}userinfo  <utente> (Se non si inserisce un utente il bot restituirà le info dell\'autore del comando)',
                         inline=False)
         embed.add_field(name='serverinfo',
-                        value='r-serverinfo',
+                        value=f'{ctx.prefix}serverinfo',
                         inline=False)
-        embed.add_field(name='say', value='r-say [messaggio]', inline=False)
-        embed.add_field(name='comment', value='r-comment [commento youtube] -- <id utente>')
+        embed.add_field(name='say', value=f'{ctx.prefix}say [messaggio]', inline=False)
+        embed.add_field(name='comment', value=f'{ctx.prefix}comment [commento youtube] -- <id utente>')
         await ctx.send(embed=embed)
 
 
