4a82d89a3049e11c3a7f7f3d98acac696c8e1794
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2021-03-25 19:26:14 +0100

    update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,11 @@ import jishaku
 import prefix
 import dotenv as envfiles
 import server
+from discord_slash import SlashCommand
 
 bot = commands.Bot(command_prefix=prefix.get_prefix, intents=discord.Intents.all())
+slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through the client.
+
 print(discord)
 bot.remove_command('help')
 bot.load_extension('jishaku')
@@ -15,6 +18,14 @@ bot.load_extension('jishaku')
 async def on_ready():
     print('Online come', bot.user)
     await bot.change_presence(activity=discord.Streaming(name='r-help per una lista di comandi | rispondo a tutto e tutti', url='https://twitch.tv/starnumber12046'))
+
+guild_ids = [] # Put your server ID in this array.
+for a in bot.guilds:
+  guild_ids.append(a.id)
+@slash.slash(name="ping", guild_id=guild_ids)
+async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
+    await ctx.respond()
+    await ctx.send(f"Pong! ({client.latency*1000}ms)")
 
 for a in os.listdir("./cogs"):
     if a.endswith(".py"):
