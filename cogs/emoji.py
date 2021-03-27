929f695efbe9db6bc5ddf2ed2ab1266bf9943187
StarNumber12046 <francesco.lumaca2009@gmail.com>
2021-03-27 13:22:08 +0100

    some edits

---
+++ cogs/emoji.py
@@ -0,0 +1,37 @@
+import discord, aiosqlite, random
+from discord.ext import commands
+
+class Emoji(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group()
+    async def emotechannel(self, ctx, channel=discord.TextChannel):
+        con = await aiosqlite.connect("./data/misc.db")
+        found = False
+        async with await con.execute("SELECT * from emojis") as cursor:
+            async for row in cursor:
+                if row[0] == ctx.guild.id:
+                    found = True
+                    break
+        if found:
+            await con.execute(f"UPDATE emojis set channel = {channel.id} WHERE guild = {ctx.guild.id}")
+        else:
+            await con.execute("INSERT into emojis (guild, channel) VALUES (?, ?)", (ctx.guild.id, channel.id))
+        await con.close()
+
+    @emotechannel.command()
+    async def remove(self, ctx):
+        con = await aiosqlite.connect("./data/misc.db")
+        found = False
+        async with await con.execute("SELECT * from emojis") as cursor:
+            async for row in cursor:
+                if row[0] == ctx.guild.id:
+                    found = True
+                    break
+
+        if found:
+            await con.execute(f"DELETE FROM emojis where guild = {ctx.guild.id}")
+
+        else:
+            await ctx.send("Non hai impostato un canale!!!")
