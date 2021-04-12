88bebf525fa39c28eba4570cc6fe88bed69d41b8
StarNumber12046 <francesco.lumaca2009@gmail.com>
2021-04-12 14:50:57 +0200

    novità

--- cogs/errorspy.py
+++ cogs/errorspy.py
@@ -94,6 +94,8 @@ class errori(commands.Cog):
             await ctx.send(embed=embed)
         elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Una parte del comando **manca**")
+        else:
+            await ctx.send("Si è verificato un errore sconosciuto. Verifica di aver inserito correttamente i dati e riprova. Se il problema persiste entra nel server di supporto")
         raise error
 
 
