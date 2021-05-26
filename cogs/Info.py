be0db37536ee4492bd9313c227916e8e32a53c23
StarNumber12046 <francesco.lumaca2009@gmail.com>
2021-05-26 21:31:42 +0200

    che belli i tasti

--- cogs/Info.py
+++ cogs/Info.py
@@ -1,9 +1,11 @@
 import discord
 from discord.ext import commands
+from discord_buttons import DiscordButton, Button, ButtonStyle, InteractionType
 
 class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.ddb = DiscordButton(self.bot)
 
     @commands.command()
     async def invite(self, ctx, integrer = "1911946966"):
@@ -13,5 +15,15 @@ class Info(commands.Cog):
     async def support(self, ctx):
         await ctx.send(embed=discord.Embed(title='Server di supporto', description='Chiedi **aiuto**, ottieni le **news** in anticipo e partecipa ai **giveaway**!',
                                            url='https://discord.gg/EzT4zT4'))
+    @commands.command()
+    async def infos(self, ctx):
+        integrer = "1911946966"
+        m = await ctx.send("Menu", buttons=[Button(style=ButtonStyle.URL, label="Invito", url=f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions={integrer}&scope=bot"), Button(style=ButtonStyle.blue, label="Bot")])
+        res = await self.ddb.wait_for_button_click(m)
+        await res.respond(
+            type=InteractionType.ChannelMessageWithSource,
+            content=f'Questo bot è stato creato da StarNumber12046. E\'un piccolo bot italiano. Ecco alcune informazioni\nPing: {self.bot.latency * 1000}\nServers: {len(self.bot.guilds)}\nUtenti: {len(self.bot.users)}'
+        )
+
 def setup(bot):
     bot.add_cog(Info(bot))
