64623add31ac470ee8caa2810a92e59a1abed8d5
StarNumber12046 <francesco.lumaca2009@gmail.com>
2021-08-10 18:31:35 +0200

    new things that u love

--- cogs/help.py
+++ cogs/help.py
@@ -1,5 +1,5 @@
 import aiosqlite
-import discord
+import discord, random
 from discord.ext import commands
 from discord_components import DiscordComponents, Button, ButtonStyle, InteractionType
 
@@ -20,78 +20,164 @@ def is_blacklisted(ctx):
     return commands.check(predicate)
 
 
+class HelpCommand:
+    def __init__(self, bot):
+        self.bot = bot
+
+    def get_type(self, command):
+        "check if a command or a cog is passed"
+
+        if self.bot.get_command(command):
+            return "command"
+
+        elif self.bot.get_cog(command):
+            return "cog"
+
+        else:
+            return None
+
+    async def command_not_found(self, ctx, command):
+        "command not found error"
+        emb = discord.Embed(description=f"```\ncommand {command} not found!\n```", colour=discord.Colour.red())
+        return await ctx.send(embed=emb)
+
+    async def cog_not_found(self, ctx, cog):
+        "command not found error"
+        emb = discord.Embed(description=f"```\ncog {cog} not found!\n```", colour=discord.Colour.red())
+        return await ctx.send(embed=emb)
+
+    async def command_list(self, ctx):
+        "return the commands list"
+
+        emb = discord.Embed(title="Help",
+                            timestamp=ctx.message.created_at)
+        emb.set_author(name=ctx.author, icon_url=str(ctx.author.avatar_url_as(static_format="png")))
+
+        for cog in self.bot.cogs:
+            cog_str = ""
+            self.bot.clean_prefix = ctx.prefix
+            cog = self.bot.get_cog(cog)
+            commands = cog.get_commands()
+            commands = [cmd for cmd in commands if not cmd.hidden]
+
+            if len(commands) >= 1:
+                for command in commands:
+                    cog_str += f"{self.bot.clean_prefix}{command.name} {command.signature}\n" if command.signature else f"{self.bot.clean_prefix}{command.name}\n"
+
+                    try:
+                        for cmd in command.commands:
+                            cog_str += f"{self.bot.clean_prefix}{cmd.parent} {cmd.name}{cmd.signature}\n" if command.signature else f"{self.bot.clean_prefix}{cmd.parent} {cmd.name}\n"
+                    except:
+                        pass
+
+                emb.add_field(name=cog.qualified_name, value=f"```prolog\n{cog_str}\n```")
+
+        return await ctx.send(embed=emb)
+
+    async def command(self, ctx, command: commands.Command):
+        "return single command help"
+
+        if command.hidden:
+            return await self.command_not_found(ctx, command.name)
+
+        emb = discord.Embed(title="Help", description=command.help,
+                            colour=discord.Colour.from_hsv(random.random(), 1, 1), timestamp=ctx.message.created_at)
+        emb.set_author(name=ctx.author, icon_url=str(ctx.author.avatar_url_as(static_format="png")))
+
+        try:
+            parent = command.parent
+        except:
+            parent = None
+
+        if parent:
+            usage = f"```{self.bot.clean_prefix}{command.parent} {command.name}{command.signature}```" if command.signature else f"```{self.bot.clean_prefix}{command.parent} {command.name}```"
+            emb.add_field(name="Usage", value=usage)
+
+            if command.aliases:
+                aliases = "\n".join([f"{self.bot.clean_prefix}{cmd}" for cmd in command.aliases])
+                emb.add_field(name="Aliases", value='```\n{}\n```'.format(aliases))
+
+            emb.add_field(name="Parent", value=f"```\n{self.bot.clean_prefix}{command.parent}\n```")
+
+            return await ctx.send(embed=emb)
+
+        else:
+            usage = f"```{self.bot.clean_prefix}{command.name} {command.signature}```" if command.signature else f"```{self.bot.clean_prefix}{command.name}```"
+            emb.add_field(name="Usage", value=usage)
+
+            if command.aliases:
+                aliases = "\n".join([f"{self.bot.clean_prefix}{cmd}" for cmd in command.aliases])
+                emb.add_field(name="Aliases", value='```\n{}\n```'.format(aliases))
+
+            try:
+                if command.commands:
+                    subcommands = ""
+                    for cmd in [c for c in command.commands if not c.hidden]:
+                        subcommands += f"{self.bot.clean_prefix}{cmd.parent} {cmd.name} {cmd.signature}\n"
+
+                    emb.add_field(name="Subcommands", value=f"```\n{subcommands}\n```")
+
+            except:
+                pass
+
+            return await ctx.send(embed=emb)
+
+    async def cog(self, ctx, cog: commands.Cog):
+        "return cog commands"
+
+        emb = discord.Embed(title=cog.qualified_name, description="",
+                            colour=discord.Colour.from_hsv(random.random(), 1, 1), timestamp=ctx.message.created_at)
+        emb.set_author(name=ctx.author, icon_url=str(ctx.author.avatar_url_as(static_format="png")))
+
+        commands = cog.get_commands()
+        commands = [cmd for cmd in commands if not cmd.hidden]
+
+        cog_str = ""
+
+        if len(commands) >= 1:
+            for command in commands:
+                cog_str += f"{self.bot.clean_prefix}{command.name} {command.signature}\n" if command.signature else f"{self.bot.clean_prefix}{command.name}\n"
+
+                try:
+                    for cmd in command.commands:
+                        cog_str += f"{self.bot.clean_prefix}{cmd.parent} {cmd.name} {cmd.signature}\n" if command.signature else f"{self.bot.clean_prefix}{cmd.parent} {cmd.name}\n"
+                except:
+                    pass
+
+            emb.description = f"```prolog\n{cog_str}\n```"
+
+        else:
+            return await self.cog_not_found(ctx, cog.qualified_name)
+
+        return await ctx.send(embed=emb)
 class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         DiscordComponents(bot)
+        self._original_help_command = bot.help_command
+        self.help = HelpCommand(bot)
+        bot.help_command = None
 
-    @commands.group(invoke_without_command=True)
-    async def help(self, ctx):
-        embed = discord.Embed(title='Aiuto', description=None, color=discord.Color.blurple())
-        embed.add_field(name='Moderazione', value=f'{ctx.prefix}help moderator | Modera il tuo server', inline=False)
-        embed.add_field(name='Misc', value=f'{ctx.prefix}help misc | Comandi inutili ma divertenti', inline=False)
-        embed.add_field(name='Info', value=f'{ctx.prefix}help info | Info sul bot', inline=False)
-        embed.add_field(name='Utility', value=f'{ctx.prefix}help utility | Comandi utili', inline=False)
-        embed.set_footer(text='C\'è un comando segreto che devi assolutamente trovare! E se lo trovi dillo al mio '
-                              'owner StarNumber12046#9008')
-        await ctx.send(embed=embed)
+    @commands.command(hidden=True)
+    async def help(self, ctx, command=None):
+        "stop it, get some help"
 
-    @help.command()
-    async def moderator(self, ctx):
-        embed = discord.Embed(title='Moderazione', description='<> = non obbligatorio | [] = obbligatorio',
-                              color=discord.Color.blurple())
-        embed.add_field(name='ban', value=f'{ctx.prefix}ban [user] <reason> (Autorizzazione richiesta: bannare membri)',
-                        inline=False)
-        embed.add_field(name='kick',
-                        value=f'{ctx.prefix}kick [user] <reason> (Autorizzazione richiesta: espellere membri)',
-                        inline=False)
-        embed.add_field(name='unban', value=f'{ctx.prefix}unban [user] (Autorizzazione richiesta: bannare membri)',
-                        inline=False)
-        embed.add_field(name="warn",
-                        value=f"{ctx.prefix}warn [user] reason (autorizzazione richiesta: gestire messaggi)\navvisa un utente")
-        embed.add_field(name="warns", value=f"{ctx.prefix}warns <user>\nvisualizza avvisi di un utente")
-        embed.add_field(name="removewarn",
-                        value=f"{ctx.prefix}removewarn [id] (autorizzazione richiesta: gestire messaggi)\nrimuove un avviso ad un utente")
-        await ctx.send(embed=embed)
+        if not command:
+            await self.help.command_list(ctx)
 
-    @help.command()
-    async def utility(self, ctx):
-        embed = discord.Embed(title='Utility', description='<> = non obbligatorio | [] = obbligatorio',
-                              color=discord.Color.blurple())
-        embed.add_field(name='prefix / setprefix',
-                        value=f'{ctx.prefix}setprefix/prefix [nuovo prefisso/reset] \nN.B. reset resetta ad **r-** il prefisso',
-                        inline=False)
-        embed.add_field(name=f"poll",
-                        value=f"{ctx.prefix}poll [titolo sondaggio] | <opzione 1> - <opzione 2> \nN.B. è possibile aggiungere infinite opzioni ma devono essere separate da un **-** . Se non si inseriscono le opzioni sarà inviato un sondaggio con :thumbsup: e :thumbsdown:",
-                        inline=False)
-        await ctx.send(embed=embed)
+        else:
+            if self.help.get_type(command) == "command":
+                await self.help.command(ctx, self.bot.get_command(command))
 
-    @help.command()
-    async def info(self, ctx):
-        embed = discord.Embed(title='Info sul bot', description='[] = obbligatorio, <> = non obbligatorio',
-                              color=discord.Color.blurple())
-        embed.add_field(name='Invite',
-                        value=f'{ctx.prefix}invite <permissions integrer> (se non si scrive nulla il bot userà i permessi consigliati)',
-                        inline=False)
-        embed.add_field(name='Support', value=f'{ctx.prefix}support', inline=False)
-        await ctx.send(embed=embed)
+            elif self.help.get_type(command) == "cog":
+                await self.help.cog(ctx, self.bot.get_cog(command))
 
-    @help.command()
-    async def misc(self, ctx):
-        embed = discord.Embed(title='Misc', description='[] = obbligatorio | <> = non obbligatorio',
-                              color=discord.Color.blurple())
-        embed.add_field(name='userinfo',
-                        value=f'{ctx.prefix}userinfo  <utente> (Se non si inserisce un utente il bot restituirà le info dell\'autore del comando)',
-                        inline=False)
-        embed.add_field(name='serverinfo',
-                        value=f'{ctx.prefix}serverinfo',
-                        inline=False)
-        embed.add_field(name='say', value=f'{ctx.prefix}say [messaggio]', inline=False)
-        embed.add_field(name='comment', value=f'{ctx.prefix}comment [commento youtube] -- <id utente>')
-        embed.add_field(name="meme", value=f"{ctx.prefix}meme", inline=False)
-        await ctx.send(embed=embed)
+            else:
+                await self.help.command_not_found(ctx, command)
 
-    @commands.command()
+
+
+    @commands.command(hidden=True)
     async def ihelp(self, ctx):
         m = await ctx.send("Help menu", components=[Button(style=ButtonStyle.blue, label="Moderazione"),
                                                  Button(style=ButtonStyle.blue, label="Misc"),
@@ -158,7 +244,7 @@ class Help(commands.Cog):
                 type=InteractionType.ChannelMessageWithSource,
                 content=f'Menu inviato'
             )
-    @commands.command()
+    @commands.command(hidden=True)
     async def testpoll(self, ctx, *, poll):
         await ctx.send(embed=discord.Embed(title="Sondaggio", description=poll), components=[Button(style=ButtonStyle.grey, label="", emoji="👍"), Button(style=ButtonStyle.grey, label="", emoji="👎")])
     @commands.Cog.listener()
--- cogs/spotify.py
+++ cogs/spotify.py
@@ -1,4 +1,4 @@
-import discord
+import discord, datetime
 from discord.ext import commands
 
 class SpotifySync(commands.Cog):
@@ -11,15 +11,28 @@ class SpotifySync(commands.Cog):
     async def spotify(self, ctx, user:discord.Member=None):
         user = user or ctx.author
         activity = user.activity
-        if isinstance(user.activity, discord.Spotify):
-            emb = discord.Embed(title=f"{str(user)} | {activity.title} di {activity.artist}", description=f"<@!{user.id}>> sta ascoltando da {str(activity.start)} ({activity.duration})")
-            emb.add_field(name="Artista", value=activity.artists)
-            emb.add_field(name="Album", value=activity.album)
-            emb.color = activity.color
-            emb.set_thumbnail(url=activity.album_cover_url)
-            await ctx.send(embed=emb)
-        else:
-            await ctx.send(embed=discord.Embed(title=f"{str(ctx.author)}, abbiamo un problema!", description=f"<@!{user.id}> non sta ascoltando nulla su Spotify!"))
+
+        for a in user.activities:
+            print(a)
+
+            if isinstance(a, discord.Spotify):
+                emb = discord.Embed(title=f"{str(user)} | {a.title} di {a.artist}", description=f"")
+                emb.url = "https://open.spotify.com/track/" + a.track_id
+                artists = " | ".join(a.artists)
+                emb.add_field(name="Artisti", value=artists)
+                emb.add_field(name="Album", value=a.album)
+                emb.color = a.color
+
+                if a.duration:
+                    emb.add_field(name="Durata", value=a.duration, inline=False)
+                emb.set_thumbnail(url=a.album_cover_url)
+                return await ctx.send(embed=emb)
+
+        print("No user activity")
+        return await ctx.send(embed=discord.Embed(title=f"{str(ctx.author)}, abbiamo un problema!", description=f"<@!{user.id}> non sta ascoltando nulla su Spotify!"))
+
+
+
 
 
 
